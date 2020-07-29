c9754d3e0f8517918859f16ae798276a5836826c
songjie <songjie1120@g.ncu.edu.tw>
2020-07-29 08:35:53 +0800

    validation

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,17 @@
     <v-col cols="8" class="mt-16">
       <v-row align="baseline" justify="center">
         <v-col md="8">
-          <v-text-field 
-            outlined 
-            placeholder="please input the amount of pokemons."
-            v-model="pokemonsAmount"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="auto">
-          <v-btn color="primary" @click="fetchPokemons" :disabled="loading" :loading="loading">submit</v-btn>
+          <v-form ref="amountForm">
+            <v-text-field 
+              outlined 
+              placeholder="Please input the amount of pokemons."
+              :rules="numberOnlyRule"
+              v-model="pokemonsAmount"
+            ></v-text-field>
+          </v-form>
+          <v-col cols="auto">
+            <v-btn color="primary" @click="fetchPokemons" :disabled="loading" :loading="loading">submit</v-btn>
+          </v-col>
         </v-col>
       </v-row>      
     </v-col>
@@ -20,31 +23,30 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   methods:{
     fetchPokemons() {
-      this.loading=true
-      this.$store.dispatch('pokemons/fetchPokemons',{first:this.pokemonsAmount})
-        .then(()=>{
-          this.loading=false
-          this.$router.push({
-              path: '/pokemons'
-          })
-        })
+      if(this.$refs.amountForm.validate()){
+        this.loading=true
+        this.$store.dispatch('pokemons/fetchPokemons',{first:this.pokemonsAmount})
+          .then(()=>{
+            this.loading=false
+            this.$router.push({
+                path: '/pokemons'
+            })
+          })  
+      }
     }
   },
   data(){
     return{
       pokemonsAmount: null,
-      loading:false
+      loading: false,
+      valid: true,
+      numberOnlyRule: [
+        v =>  v > 0  || 'Please input a positive number.',
+      ]
     }
-  },
-  components: {
-    Logo,
-    VuetifyLogo
-  },
+  }
 }
 </script>
